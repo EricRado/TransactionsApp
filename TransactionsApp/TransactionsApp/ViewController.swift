d959734df0a84747fcf35cff7c77e9b8d54fe216
Eric-Rado <eric.rado@whoop.com>
2021-08-07 11:14:33 -0400

    Project initialization

---
+++ TransactionsApp/TransactionsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TransactionsApp
+//
+//  Created by Eric Rado on 8/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
